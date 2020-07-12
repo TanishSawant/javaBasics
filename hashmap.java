9cf3d77054cc265402eb6e5f2c224601d0a5d26c
Tanish Sawant <tanishsawant@rediffmail.com>
2020-07-12 11:07:37 +0530

    hashmap

---
+++ hashmap.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class hashmap {
+    
+    public static void main(String[] args) {
+        HashMap <String, String> map1 = new HashMap<String, String>();
+        map1.put("India", "Delhi");
+        map1.put("Spain", "Madrid");
+        map1.put("Germany", "Berlin");
+        map1.put("England", "london");
+
+        System.out.println(map1.get("Germany"));
+    }
+}
